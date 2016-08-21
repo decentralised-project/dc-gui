f862d7c88a978af8416e06800762ba2cb61c20f6
decentralised-project <hello@decentralised-project.org>
2016-08-21 21:51:59 +0100

    crypto net manager wip

---
+++ dependencies/dccrypto/include/crypt_network_manager.hpp
@@ -0,0 +1,42 @@
+#ifndef __CRYPT_NETWORK_MANAGER_H_INCLUDED__
+#define __CRYPT_NETWORK_MANAGER_H_INCLUDED__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <memory>
+
+#include <openssl/bn.h>
+#include <openssl/ec.h>
+#include <openssl/obj_mac.h>
+#include <openssl/ecdsa.h>
+#include <openssl/bio.h>
+#include <openssl/err.h>
+#include <openssl/pem.h>
+
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+#include "p2p_manager.hpp"
+
+namespace dccrypto
+{
+	class crypt_network_manager : public dcp2p::p2p_manager, public boost::enable_shared_from_this<crypt_network_manager>
+	{
+	public:
+		typedef boost::shared_ptr<crypt_network_manager> pointer;
+
+		static pointer Create()
+		{
+			return pointer(new crypt_network_manager());
+		}
+
+	private:
+		crypt_network_manager();
+	};
+}
+
+#endif
---
+++ dependencies/dccrypto/src/crypt_network_manager.cpp
@@ -0,0 +1,8 @@
+#include "crypt_network_manager.hpp"
+
+namespace dccrypto
+{
+	crypt_network_manager::crypt_network_manager()
+	{
+	}
+}
